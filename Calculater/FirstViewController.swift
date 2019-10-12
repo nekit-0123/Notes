9cc5d45e3c30bd93626c4b77debba57de2fddbbd
Никита <nikitayudichev@macbook-pro-nikita.local>
2019-10-12 14:02:57 +0300

    Initial Commit

---
+++ Calculater/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Calculater
+//
+//  Created by Nikita Yudichev on 12.10.2019.
+//  Copyright © 2019 Nikita Yudichev. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Calculater/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Calculater
+//
+//  Created by Nikita Yudichev on 12.10.2019.
+//  Copyright © 2019 Nikita Yudichev. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
